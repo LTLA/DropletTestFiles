6b824f66604034383e0c4b189819e93d0d58668c
jonathangriffiths <7976085+jonathangriffiths@users.noreply.github.com>
2020-08-25 17:18:02 +0100

    Changes to match package style

--- inst/scripts/1.0.0/make_bach.R
+++ inst/scripts/1.0.0/make_bach.R
@@ -10,27 +10,33 @@ system2("mv", paste("data/molecule_info/*", target))
 
 orig_files <- dir(target, recursive = TRUE)
 
-#name tweak for consistency
+#name tweaks for consistency
 files <- dir(file.path(target, "hiseq_2500_run2"), full.names = TRUE)
+system2("mv", c(file.path(target, "hiseq_2500_run2"),
+                file.path(target, "hiseq_2500_exp2")))
+files <- dir(file.path(target, "hiseq_2500_exp2"), full.names = TRUE)
 file.rename(
-    files, 
-    file.path(dirname(files), paste0("molecule_info_", basename(files)))
+    files,
+    file.path(dirname(files), paste0("mol_info_", basename(files)))
 )
+files <- dir(target, recursive = TRUE, full.names = TRUE)
+file.rename(files, gsub("molecule", "mol", files))
+
 
 #metadata
 files <- dir(target, recursive = TRUE, full.names = TRUE)
 sequencer <- ifelse(grepl("4000", files), "HiSeq 4000", "HiSeq 2500")
-experiment <- ifelse(grepl("run2", files), 2, 1)
+experiment <- ifelse(grepl("exp2", files), 2, 1)
 barcode <- regmatches(files, regexpr("[A-Z][1-9]", files))
-title <- paste0(sequencer, "-sequenced scRNAseq mammary epithelial cells (expt. ", experiment, " barcode ", barcode, ")")
-desc <- paste0("Molecule info file for ", sequencer, "-sequenced scRNAseq molecule info (experiment ", experiment, ", using 10x barcode ", barcode, "; 10x 3' v2 kit)")
+title <- paste0(sequencer, "-sequenced mammary epithelial cells (expt. ", experiment, " barcode ", barcode, ")")
+desc <- paste0("Molecule information file for ", sequencer, "-sequenced single-cell RNA-seq data (experiment ", experiment, ", using 10x barcode ", barcode, "; 10x 3' v2 kit)")
 meta <- data.frame(
     Title = title,
     Description = desc,
     RDataPath = files,
     BiocVersion=3.12,
     Genome="mm10",
-    SourceType="H5",
+    SourceType="HDF5",
     SourceUrl="https://jmlab-gitlab.cruk.cam.ac.uk/Jonny/BarcodeSwapping2017Data/raw/master/bcswap_data.tar.gz",
     SourceVersion=orig_files,
     Species="Mus musculus",
@@ -39,7 +45,7 @@ meta <- data.frame(
     DataProvider="Jonathan Griffiths",
     Maintainer="Aaron Lun <infinite.monkeys.with.keyboards@gmail.com>",
     RDataClass="character",
-    DispatchClass="H5File",
+    DispatchClass="FilePath",
     stringsAsFactors = FALSE
 )
 write.csv(meta, row.names=FALSE,
